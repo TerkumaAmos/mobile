df0c0af91ba1fe8fe3feeb3a430129817b062727
TerkumaAmos <agberterkuma2020@gmail.com>
2025-03-17 13:10:54 +0100

    new

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -6,28 +6,55 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 10.0),
-          child: CircleAvatar(
-            backgroundColor: Colors.white,
-            child: Center(
-              child: IconButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                icon: const BackButton(
-                  style: ButtonStyle(iconSize: WidgetStatePropertyAll(20)),
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Center(
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const BackButton(
+                    style: ButtonStyle(iconSize: WidgetStatePropertyAll(20)),
+                  ),
                 ),
               ),
             ),
           ),
+          title: const Text(
+            "My Account",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-        title: const Text(
-          "My Account",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        body: Column(children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 250,
+                        width: 400,
+                        margin: const EdgeInsets.only(left: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.red,
+                        ),
+                      ),
+                      Container(
+                        height: 250,
+                        width: 400,
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ))),
+        ]));
   }
 }
