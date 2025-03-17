87b0f64145196e8a778a4fe7c7db0b2d45d2e052
TerkumaAmos <agberterkuma2020@gmail.com>
2025-03-17 15:27:46 +0100

    new

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/colors.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
@@ -6,55 +7,87 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 10.0),
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Center(
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  icon: const BackButton(
-                    style: ButtonStyle(iconSize: WidgetStatePropertyAll(20)),
-                  ),
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Center(
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const BackButton(
+                  style: ButtonStyle(iconSize: WidgetStatePropertyAll(20)),
                 ),
               ),
             ),
           ),
-          title: const Text(
-            "My Account",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
         ),
-        body: Column(children: [
+        title: const Text(
+          "My Account",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(
+        children: [
           Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 250,
-                        width: 400,
-                        margin: const EdgeInsets.only(left: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.red,
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    height: 250,
+                    width: 400,
+                    margin: const EdgeInsets.only(left: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: AppColors.bgColor,
+                    ),
+                    child: const Stack(
+                      children: [
+                        Positioned(
+                          top: 30,
+                          left: 30,
+                          child: Text(
+                            "Jane Smith",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
-                      ),
-                      Container(
-                        height: 250,
-                        width: 400,
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.red,
+                        Positioned(
+                          left: 400,
+                          child: SizedBox(
+                            height: 30,
+                            width: 40,
+                            child: Image(
+                              fit: BoxFit.cover,
+                              image: AssetImage(
+                                "assets/credit2.jpeg",
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
-                  ))),
-        ]));
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 250,
+                    width: 400,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
